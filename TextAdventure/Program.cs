84879641c3a40d9c57278fbcbf2cf40c6a0d0d54
Alec <0704estrada@gmail.com>
2021-01-18 21:51:06 -0600

    First Commit

---
+++ TextAdventure/Program.cs
@@ -0,0 +1,38 @@
+﻿/*  Alec Estrada -- Text Adventure Game  */
+using System;
+
+namespace TextAdventure
+{
+    class Program
+    {
+        public static Player currentPlayer = new Player();
+
+        static void Main(string[] args)
+        {
+            Start();
+        }
+
+
+
+        static void Start()
+        {
+            Console.WriteLine("The fallen Dungeon!!");
+            Console.WriteLine("Name: ");
+            
+            currentPlayer.name = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("A light shining bright, almost if the rays of the sun were upon you at birth.");
+            Console.WriteLine("You awake to a group of people dressed in light colored robes and a circle that glows with every breath you take.");
+            if (currentPlayer.name == "")
+                Console.Write("I see you don't even remeber your own name, but still no name you will save this world.");
+            else
+                Console.WriteLine("Hello, " + currentPlayer.name + " we have summoned you to save this world.");
+
+            Console.ReadKey();
+        }
+
+
+    }
+
+
+}
